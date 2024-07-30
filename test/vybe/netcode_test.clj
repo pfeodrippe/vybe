9bc302530ff5d82043f13cef7fb35e233fd732bb
Paulo Feodrippe <pfeodrippe@gmail.com>
2024-07-29 22:56:31 -0400

    Don't run netcode test in linux

--- test/vybe/netcode_test.clj
+++ test/vybe/netcode_test.clj
@@ -1,8 +1,13 @@
 (ns vybe.netcode-test
   (:require
    [clojure.test :refer [deftest testing is]]
-   [vybe.netcode :as vn]))
+   [vybe.panama :as vp]))
 
-(deftest init-close-test
-  (is (= 1 (vn/init!)))
-  (is (= nil (vn/close!))))
+(when-not vp/linux?
+  ;; In the CI for Linux, we have some linker issue.
+  (eval
+   '(do
+      (require '[vybe.netcode :as vn])
+      (deftest init-close-test
+        (is (= 1 (vn/init!)))
+        (is (= nil (vn/close!)))))))
