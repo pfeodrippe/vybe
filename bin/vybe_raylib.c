51adf75d1c9ce2b0b423c73c3d7362cc5f62ad1f
Paulo Feodrippe <pfeodrippe@gmail.com>
2024-08-19 23:48:39 -0400

    Try to fix windows

--- bin/vybe_raylib.c
+++ bin/vybe_raylib.c
@@ -5,4 +5,14 @@
 #if !defined(_WIN32)
   #define RAYGUI_IMPLEMENTATION TRUE
 #endif
+
 #include "../raygui/src/raygui.h"
+
+#if defined(_WIN32)
+RAYGUIAPI void GuiLoadStyleCherry(void);
+RAYGUIAPI void GuiLoadStyleTerminal(void);
+RAYGUIAPI void GuiLoadStyleSunny(void);
+RAYGUIAPI void GuiLoadStyleCandy(void);
+RAYGUIAPI void GuiLoadStyleAshes(void);
+RAYGUIAPI void GuiLoadStyleEnefet(void);
+#endif
