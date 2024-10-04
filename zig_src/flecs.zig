28e302a3e7994c7613877db0035ff7cf85426606
Paulo Feodrippe <pfeodrippe@gmail.com>
2024-10-03 21:01:27 -0400

    [zig] Call each

--- zig_src/flecs.zig
+++ zig_src/flecs.zig
@@ -1,11 +1,14 @@
 const std = @import("std");
 
-const f = @import("flecs_c.zig");
-const world_t = f.ecs_world_t;
-const entity_t = f.ecs_entity_t;
+const fc = @import("flecs_c.zig");
+const world_t = fc.ecs_world_t;
+const entity_t = fc.ecs_entity_t;
+const entity_desc_t = fc.ecs_entity_desc_t;
+const system_desc_t = fc.ecs_system_desc_t;
+const iter_t = fc.ecs_iter_t;
 
-pub fn field(it: *f.ecs_iter_t, comptime T: type, index: i8) ?[]T {
-    if (f.ecs_field_w_size(it, @sizeOf(T), index)) |anyptr| {
+pub fn field(it: *fc.ecs_iter_t, comptime T: type, index: i8) ?[]T {
+    if (fc.ecs_field_w_size(it, @sizeOf(T), index)) |anyptr| {
         const ptr = @as([*]T, @ptrCast(@alignCast(anyptr)));
         return ptr[0..@intCast(it.count)];
     }
@@ -16,12 +19,12 @@ pub const Eid = struct { name: []u8 };
 
 fn _eid(wptr: *world_t, T: anytype) entity_t {
     if (@TypeOf(T) == type) {
-        const entity = f.ecs_entity_init(wptr, &.{
+        const entity = fc.ecs_entity_init(wptr, &.{
             .use_low_id = true,
             .name = @typeName(T),
         });
 
-        _ = f.ecs_component_init(
+        _ = fc.ecs_component_init(
             wptr,
             &.{
                 .entity = entity,
@@ -37,11 +40,11 @@ fn _eid(wptr: *world_t, T: anytype) entity_t {
 
     switch (@typeInfo(@TypeOf(T))) {
         .Pointer => |_| {
-            return f.ecs_entity_init(wptr, &.{ .name = T });
+            return fc.ecs_entity_init(wptr, &.{ .name = T });
         },
         .Struct => |info| {
             if (info.fields.len == 0) {
-                return f.ecs_entity_init(wptr, &.{});
+                return fc.ecs_entity_init(wptr, &.{});
             }
             if (info.is_tuple) {
                 if (info.fields.len != 2) {
@@ -55,17 +58,17 @@ fn _eid(wptr: *world_t, T: anytype) entity_t {
                     _eid(wptr, T[1]),
                 );
             }
-            var desc = f.ecs_entity_desc_t{};
+            var desc = fc.ecs_entity_desc_t{};
             desc.name = T.name;
             if (@hasField(@TypeOf(T), "parent")) {
                 if (T.parent != null) {
                     desc.parent = T.parent.?;
                 }
             }
-            return f.ecs_entity_init(wptr, &desc);
+            return fc.ecs_entity_init(wptr, &desc);
         },
         .EnumLiteral => |_| {
-            return f.ecs_entity_init(wptr, &.{ .name = @tagName(T) });
+            return fc.ecs_entity_init(wptr, &.{ .name = @tagName(T) });
         },
         .Int => |_| {
             return T;
@@ -83,7 +86,7 @@ const Entity = struct {
 
     pub fn has_id(self: Entity, id: anytype) bool {
         const w = self.w;
-        return f.ecs_has_id(
+        return fc.ecs_has_id(
             w.wptr,
             w.eid(self.id),
             w.eid(id),
@@ -93,7 +96,7 @@ const Entity = struct {
     pub fn get(self: Entity, comptime T: type) ?T {
         const w = self.w;
         const entity = self.id;
-        if (f.ecs_get_id(w.wptr, w.eid(entity), w.eid(T))) |ptr| {
+        if (fc.ecs_get_id(w.wptr, w.eid(entity), w.eid(T))) |ptr| {
             return cast(T, ptr).*;
         }
         return null;
@@ -103,7 +106,7 @@ const Entity = struct {
         const w = self.w;
         const entity = self.id;
         if (T == *const anyopaque) {
-            f.ecs_set_id(
+            fc.ecs_set_id(
                 w.wptr,
                 entity,
                 w.eid(T),
@@ -111,7 +114,7 @@ const Entity = struct {
                 data,
             );
         } else {
-            f.ecs_set_id(
+            fc.ecs_set_id(
                 w.wptr,
                 entity,
                 w.eid(T),
@@ -124,7 +127,7 @@ const Entity = struct {
     pub fn add_id(self: Entity, id: anytype) void {
         const w = self.w;
         const entity = self.id;
-        f.ecs_add_id(
+        fc.ecs_add_id(
             w.wptr,
             entity,
             w.eid(id),
@@ -183,11 +186,11 @@ const World = struct {
     wptr: *world_t,
 
     pub fn new() World {
-        return World{ .wptr = f.ecs_init().? };
+        return World{ .wptr = fc.ecs_init().? };
     }
 
     pub fn close(self: World) bool {
-        return f.ecs_fini(self.wptr) == 0;
+        return fc.ecs_fini(self.wptr) == 0;
     }
 
     pub fn ent(self: World, entity: anytype) Entity {
@@ -205,17 +208,34 @@ const World = struct {
             .{ .parent = null },
         );
     }
+
+    pub fn progress(self: World, delta_time: f32) bool {
+        return fc.ecs_progress(self.wptr, delta_time);
+    }
+
+    pub fn run(self: World, system: anytype, delta_time: f32) Entity {
+        return self.ent(fc.ecs_run(
+            self.wptr,
+            self.eid(system),
+            delta_time,
+            null,
+        ));
+    }
+
+    pub fn with_system(self: World, params: SystemParams, function_struct: anytype) entity_t {
+        return _with_system(self, params, function_struct);
+    }
 };
 
 fn cast(comptime T: type, val: ?*const anyopaque) *const T {
     return @as(*const T, @ptrCast(@alignCast(val)));
 }
 
-pub fn make_pair(first: entity_t, second: entity_t) f.ecs_id_t {
-    return f.ecs_make_pair(first, second);
+pub fn make_pair(first: entity_t, second: entity_t) fc.ecs_id_t {
+    return fc.ecs_make_pair(first, second);
 }
 
-fn Move(it: *f.ecs_iter_t) callconv(.C) void {
+fn Move(it: *fc.ecs_iter_t) callconv(.C) void {
     var p = field(it, Position, 0).?;
     const v = field(it, Velocity, 1).?;
 
@@ -235,27 +255,27 @@ test "raw basics" {
     const pos_id = w.eid(Position);
     const vel_id = w.eid(Velocity);
 
-    var entity_desc = f.ecs_entity_desc_t{};
+    var entity_desc = fc.ecs_entity_desc_t{};
     entity_desc.id = w.eid(.{});
     entity_desc.name = "Move";
     entity_desc.add = &.{
-        make_pair(f.EcsDependsOn, f.EcsOnUpdate),
+        make_pair(fc.EcsDependsOn, fc.EcsOnUpdate),
         0,
     };
 
-    var system_desc = f.ecs_system_desc_t{};
+    var system_desc = fc.ecs_system_desc_t{};
     system_desc.callback = Move;
-    system_desc.entity = f.ecs_entity_init(w.wptr, &entity_desc);
+    system_desc.entity = fc.ecs_entity_init(w.wptr, &entity_desc);
     system_desc.query.terms[0] = .{
         .id = pos_id,
-        .inout = f.EcsInOut,
+        .inout = fc.EcsInOut,
     };
     system_desc.query.terms[1] = .{
         .id = vel_id,
-        .inout = f.EcsIn,
+        .inout = fc.EcsIn,
     };
 
-    const move = f.ecs_system_init(w.wptr, &system_desc);
+    const move = fc.ecs_system_init(w.wptr, &system_desc);
 
     const e1 = w.eid(.{});
     w.ent(e1).set(Position, .{ .x = 4.3, .y = 5.0 });
@@ -264,7 +284,7 @@ test "raw basics" {
     const pos_id_2 = w.eid(Position);
     try std.testing.expectEqual(pos_id, pos_id_2);
 
-    _ = f.ecs_run(w.wptr, move, @as(f32, 0.0), null);
+    _ = w.run(move, 0.0);
 
     try std.testing.expectEqual(
         Position{ .x = 14.3, .y = 20.0 },
@@ -272,39 +292,101 @@ test "raw basics" {
     );
 }
 
+fn MoveSystem(p: *Position, v: *const Velocity) void {
+    p.x += v.x;
+    p.y += v.y;
+}
+
+const SystemParams = struct {
+    name: []const u8,
+};
+
+fn _with_system(w: World, params: SystemParams, function_struct: anytype) entity_t {
+    const function = function_struct.each;
+    const fn_params = @typeInfo(@TypeOf(function)).Fn.params;
+
+    var system_desc = fc.ecs_system_desc_t{
+        .entity = fc.ecs_entity_init(w.wptr, &entity_desc_t{
+            .id = w.eid(.{}),
+            .name = params.name.ptr,
+            .add = &.{
+                make_pair(fc.EcsDependsOn, fc.EcsOnUpdate),
+                0,
+            },
+        }),
+    };
+
+    inline for (fn_params, 0..) |param, idx| {
+        const param_pointer = @typeInfo(param.type.?).Pointer;
+        const param_type = param_pointer.child;
+        const param_is_const = param_pointer.is_const;
+        const param_eid = w.eid(param_type);
+        // std.debug.print("fn param: {any}, is_const = {any}\n", .{
+        //     param_eid,
+        //     param_is_const,
+        // });
+
+        system_desc.query.terms[idx] = .{
+            .id = param_eid,
+            .inout = if (param_is_const) fc.EcsIn else fc.EcsInOut,
+        };
+    }
+
+    // Build callback.
+    const callback_struct = struct {
+        fn exec(it: *iter_t) callconv(.C) void {
+            //std.debug.print("ITER -------------==============\n", .{});
+
+            comptime var field_types: [fn_params.len]type = undefined;
+            inline for (0..fn_params.len) |idx| {
+                field_types[idx] = comptime []@typeInfo(fn_params[idx].type.?).Pointer.child;
+            }
+
+            var args_vec_tuple: std.meta.Tuple(&field_types) = undefined;
+            inline for (fn_params, 0..) |param_type, idx| {
+                args_vec_tuple[idx] = field(
+                    it,
+                    @typeInfo(param_type.type.?).Pointer.child,
+                    idx,
+                ).?;
+            }
+
+            var args_tuple: std.meta.ArgsTuple(@TypeOf(function)) = undefined;
+            for (0..@intCast(it.count)) |i| {
+                inline for (fn_params, 0..) |param, idx| {
+                    _ = param;
+                    args_tuple[idx] = &args_vec_tuple[idx][i];
+                }
+                _ = @call(.always_inline, function, args_tuple);
+            }
+        }
+    };
+    system_desc.callback = callback_struct.exec;
+
+    return fc.ecs_system_init(w.wptr, &system_desc);
+}
+
 test "preferred basics" {
     const w = World.new();
     defer _ = w.close();
 
-    const pos_id = w.eid(Position);
-    const vel_id = w.eid(Velocity);
-
-    var entity_desc = f.ecs_entity_desc_t{};
-    entity_desc.id = w.eid(.{});
-    entity_desc.name = "Move";
-    entity_desc.add = &.{
-        make_pair(f.EcsDependsOn, f.EcsOnUpdate),
-        0,
-    };
-
-    var system_desc = f.ecs_system_desc_t{};
-    system_desc.callback = Move;
-    system_desc.entity = f.ecs_entity_init(w.wptr, &entity_desc);
-    system_desc.query.terms[0] = .{
-        .id = pos_id,
-        .inout = f.EcsInOut,
-    };
-    system_desc.query.terms[1] = .{
-        .id = vel_id,
-        .inout = f.EcsIn,
-    };
-
-    const move = f.ecs_system_init(w.wptr, &system_desc);
+    _ = w.with_system(.{
+        .name = "Move",
+    }, struct {
+        fn each(p: *Position, v: *const Velocity) void {
+            p.x += v.x;
+            p.y += v.y;
+        }
+    });
 
     w.merge(.{
         .e1 = .{
             Position{ .x = 4.3, .y = 5.0 },
             Velocity{ .x = 10.0, .y = 15.0 },
+        },
+        .e0 = .{
+            Position{ .x = 2.3, .y = 4.0 },
+            Velocity{ .x = 13.0, .y = 20.0 },
         },
         .e2 = .{
             Position{ .x = 5.3, .y = 6.0 }, Velocity{ .x = 11.0, .y = 16.0 },
@@ -319,11 +401,16 @@ test "preferred basics" {
         },
     });
 
-    _ = f.ecs_run(w.wptr, move, @as(f32, 0.0), null);
+    _ = w.progress(0.0);
 
     try std.testing.expectEqual(
         Position{ .x = 14.3, .y = 20.0 },
         w.ent(.e1).get(Position).?,
+    );
+
+    try std.testing.expectEqual(
+        Position{ .x = 15.3, .y = 24.0 },
+        w.ent(.e0).get(Position).?,
     );
 
     // e10 is a child of e2.
