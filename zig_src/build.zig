35e5453b01bd6b27852d0e50c0221c4d12e8e97a
Paulo Feodrippe <pfeodrippe@gmail.com>
2024-10-06 15:05:59 -0400

    [zig] Move

--- zig_src/build.zig
+++ zig_src/build.zig
@@ -6,8 +6,8 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const vybe_lib = b.addSharedLibrary(.{
-        .name = "flecs",
-        .root_source_file = b.path("flecs.zig"),
+        .name = "zig_vybe_flecs",
+        .root_source_file = b.path("vybe/flecs.zig"),
         .target = target,
         .optimize = optimize,
         .version = .{ .major = 1, .minor = 2, .patch = 3 },
@@ -41,7 +41,7 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run unit tests");
 
     const unit_tests = b.addTest(.{
-        .root_source_file = b.path("flecs.zig"),
+        .root_source_file = b.path("vybe/flecs.zig"),
         .target = target,
     });
     unit_tests.addIncludePath(b.path("../flecs/distr"));
