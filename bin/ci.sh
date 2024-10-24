53f29ed7b1b82451d62ce2700f2f15f347bc6786
Paulo Feodrippe <pfeodrippe@gmail.com>
2024-10-23 22:58:14 -0400

    Move ATK scx file into the plugins folder for win and osx

--- bin/ci.sh
+++ bin/ci.sh
@@ -4,6 +4,9 @@ set -ex
 
 git submodule update --init --recursive
 
+mv test-resources/AtkUGens.scx sonic-pi/prebuilt/macos/universal/supercollider/Resources/plugins
+ls sonic-pi/prebuilt/macos/universal/supercollider/Resources/plugins
+
 export VYBE_JEXTRACT=jextract-22/bin/jextract
 bin/jextract-libs.sh
 
