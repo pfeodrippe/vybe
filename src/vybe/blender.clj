f00237e2c060db36eef439017364321e07f36446
Paulo Feodrippe <pfeodrippe@gmail.com>
2025-02-09 17:47:38 -0500

    [vb] Add `eval*`

--- src/vybe/blender.clj
+++ src/vybe/blender.clj
@@ -2,12 +2,13 @@
   (:require
    [clojure.edn :as edn]
    [nrepl.core :as nrepl]
-   [clojure.string :as str]
-   [clojure.pprint :as pprint]))
+   [clojure.string :as str]))
 
 (def ^:no-doc ^:dynamic *nrepl-init* nil)
 #_ (def blender-session (connect 7889))
 #_ (*nrepl-init* blender-session)
+
+(defonce *blender-session (atom nil))
 
 (defn connect
   "Connect to a running basilisp Blender nREPL server.
@@ -16,30 +17,49 @@
   with other functions from this namespace)."
   [port]
   (let [conn (nrepl/connect :port port)
-        client (nrepl/client conn Long/MAX_VALUE)]
-    {:conn conn
-     :client client}))
+        client (nrepl/client conn Long/MAX_VALUE)
+        blender-session {:conn conn
+                         :client client}]
+    (reset! *blender-session blender-session)
+    blender-session))
 #_ (def blender-session (connect 7889))
 
 (defn disconnect
   [{:keys [conn]}]
   (.close conn))
+#_ (disconnect blender-session)
+
+(defn message
+  [params {:keys [client]}]
+  (-> client
+      (nrepl/message params)))
+#_ (-> {:ns "vybe.basilisp.bake"
+        :op "complete"
+        :prefix "edn/"}
+       (message blender-session))
 
 (defn eval-str
   "Eval from the basilisp Blender session."
   ([code-str blender-session]
    (eval-str code-str blender-session {}))
   ([code-str {:keys [client]} params]
-   (let [{:keys [value err ex] :as sss} (-> client
-                                            (nrepl/message (merge {:op "eval"
-                                                                   :code code-str}
-                                                                  params))
-                                            doall
-                                            nrepl/combine-responses)
+   (let [res (-> client
+                 (nrepl/message (merge {:op "eval"
+                                        :code code-str}
+                                       params))
+                 doall)
+         {:keys [value err ex]} (-> res
+                                    nrepl/combine-responses)
          v (first value)]
      (if (not (or err ex))
-       {:value (if (str/starts-with? v "<module")
+       {:value (cond
+                 (str/starts-with? v "<module")
                  v
+
+                 (str/includes? v "bpy.")
+                 (-> v symbol pr-str)
+
+                 :else
                  (try
                    (-> v edn/read-string)
                    (catch Exception _
@@ -50,7 +70,15 @@
          (throw (ex-info "Error when evaluating basilisp code in Blender"
                          {:error {:via {:type ex
                                         :message err}}})))))))
-#_ (-> (pr-str '[(+ 2 12)])
+#_ (-> (pr-str '#'map)
+       (eval-str blender-session))
+#_ (-> (pr-str '(+ 2 12))
+       (eval-str blender-session {:ns "user"
+                                  :line 1
+                                  :column 1
+                                  :file "/Users/pfeodrippe/dev/vybe/basilisp/src/vybe/basilisp/bake.lpy"
+                                  :context :expr}))
+#_ (-> (pr-str '#'edn/read)
        (eval-str blender-session {:ns "user"
                                   :line 1
                                   :column 1
@@ -60,3 +88,20 @@
        (eval-str blender-session {:ns "vybe.basilisp.bake"}))
 #_ (-> (pr-str 'bpy.ops/objects)
        (eval-str blender-session))
+
+(defmacro eval*
+  "Eval form. It will start a blender session on port 7889 if
+  not started already."
+  ([form]
+   (when (nil? @*blender-session)
+     (connect 7889))
+   (eval* form @*blender-session))
+  ([form blender-session]
+   `(-> (pr-str ~form)
+        (eval-str ~blender-session {:ns "vybe.basilisp.bake"
+                                    :file "/Users/pfeodrippe/dev/vybe/basilisp/src/vybe/basilisp/bake.lpy"})
+        :value)))
+#_ (eval* (- 4 10))
+#_ (eval* (do (select "Scene")
+              (select "SceneOutdoors")
+              (select-only "Scene")))
--- src/vybe/nrepl.clj
+++ src/vybe/nrepl.clj
@@ -26,66 +26,84 @@
                                (println "To quit, type:" :blender/quit)
                                [:repl blender-session])))
 
-  (if-let [blender-session (and (#{"eval" "load-file"} op)
-                                (get @session #'*blender-session*))]
-    (->> (try
+  (let [blender-session (get @session #'*blender-session*)]
+    (cond
+      (and blender-session (#{"eval" "load-file"} op))
+      (->> (try
 
-           (let [[code file] (if (= "load-file" (:op msg))
-                               [(:file msg) (:file-path msg)]
-                               [(:code msg) (:file msg)])
+             (let [[code file] (if (= "load-file" (:op msg))
+                                 [(:file msg) (:file-path msg)]
+                                 [(:code msg) (:file msg)])
 
-                 {:keys [value] :as response}
-                 (vb/eval-str code
-                              blender-session
-                              (-> {:ns (get @session #'*blender-ns*)}
-                                  (merge msg)
-                                  (select-keys [:ns :line :column])
-                                  (assoc :file file
-                                         #_ #_:verbose true
-                                         :context (case op
-                                                    "eval" :expr
-                                                    "load-file" :statement)
-                                         #_ #_:re-render (= op "load-file"))))]
+                   {:keys [value] :as response}
+                   (vb/eval-str code
+                                blender-session
+                                (-> {:ns (get @session #'*blender-ns*)}
+                                    (merge msg)
+                                    (select-keys [:ns :line :column])
+                                    (assoc :file file
+                                           #_ #_:verbose true
+                                           :context (case op
+                                                      "eval" :expr
+                                                      "load-file" :statement)
+                                           #_ #_:re-render (= op "load-file"))))]
 
-             (when-let [namespace (:ns response)]
-               (swap! session assoc #'*blender-ns* namespace))
+               (when-let [namespace (:ns response)]
+                 (swap! session assoc #'*blender-ns* namespace))
 
-             (when (= value :blender/quit)
-               (println :LEAVING_BLENDER_SESSION)
-               (swap! session dissoc #'*blender-session* #'*blender-ns*))
+               (when (= value :blender/quit)
+                 (println :LEAVING_BLENDER_SESSION)
+                 (swap! session dissoc #'*blender-session* #'*blender-ns*))
 
-             (merge
-              response
-              {:status      :done
-               ::print/keys #{:value}}))
+               (merge
+                response
+                {:status      :done
+                 ::print/keys #{:value}}))
 
-           (catch Exception e
-             (swap! session assoc #'*e e)
-             {::caught/throwable e
-              :status            [:done :eval-error]
-              :ex                (str (class e)) #_ex
-              :root-ex           (str (class (main/root-cause e)))
-              :causes            (if-let [via (get-in (ex-data e) [:error :via])]
-                                   (for [{:keys [type message at]} via]
-                                     {:class      type
-                                      :message    message
-                                      :stacktrace at})
-                                   (for [ex (take-while some? (iterate ex-cause e))]
-                                     {:class      (str (class ex))
-                                      :message    (ex-message ex)
-                                      :stacktrace []}))
-              #_ #_:causes            (if-let [via (get-in (ex-data e) [:error :via])]
-                                        (for [{:keys [type message at]} via]
-                                          {:class      type
-                                           :message    message
-                                           :stacktrace at})
-                                        (for [ex (take-while some? (iterate ex-cause e))]
-                                          {:class      (str (class ex))
-                                           :message    (ex-message ex)
-                                           :stacktrace []}))}))
-         (response-for msg)
-         (transport/send transport))
-    (handler msg)))
+             (catch Exception e
+               (swap! session assoc #'*e e)
+               {::caught/throwable e
+                :status            [:done :eval-error]
+                :ex                (str (class e)) #_ex
+                :root-ex           (str (class (main/root-cause e)))
+                :causes            (if-let [via (get-in (ex-data e) [:error :via])]
+                                     (for [{:keys [type message at]} via]
+                                       {:class      type
+                                        :message    message
+                                        :stacktrace at})
+                                     (for [ex (take-while some? (iterate ex-cause e))]
+                                       {:class      (str (class ex))
+                                        :message    (ex-message ex)
+                                        :stacktrace []}))
+                #_ #_:causes            (if-let [via (get-in (ex-data e) [:error :via])]
+                                          (for [{:keys [type message at]} via]
+                                            {:class      type
+                                             :message    message
+                                             :stacktrace at})
+                                          (for [ex (take-while some? (iterate ex-cause e))]
+                                            {:class      (str (class ex))
+                                             :message    (ex-message ex)
+                                             :stacktrace []}))}))
+           (response-for msg)
+           (transport/send transport))
+
+      (and blender-session (#{"complete"} op))
+      #_blender-session
+      (let [res (-> msg
+                    ;; https://nrepl.org/nrepl/ops.html
+                    (select-keys [:ns :op :prefix :context :id :complete-fn :options
+                                  :verbose? :aux :ops :versions
+                                  :column])
+                    (vb/message blender-session))]
+        (->> (merge (first res)
+                    #_{:status :done
+                       ::print/keys #{:completions}})
+             (response-for msg)
+             (transport/send transport)))
+
+      :else
+      (let [res (handler msg)]
+        res))))
 
 (defn wrap-blender-repl
   [handler]
